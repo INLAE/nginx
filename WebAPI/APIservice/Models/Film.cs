eaca301568e67040870050032771d961721ce2b3
Ivanushkin Ivan Alexandrovich <80826763+INLAE@users.noreply.github.com>
2023-05-10 19:58:28 +0300

    Delete WebAPI/APIservice directory

--- WebAPI/APIservice/Models/Film.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WepAppLinux.Models
-{
-    public class Film
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Director { get; set; }
-    }
-}
